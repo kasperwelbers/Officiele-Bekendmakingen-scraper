372a2c4b00bdbceb9726c1a63087cedd0c8dedb2
Nel Ruigrok <nelruigrok@nieuwsmonitor.org>
2016-01-15 18:01:30 +0100

    add gitignore

--- officiele_bekendmakingen.py
+++ officiele_bekendmakingen.py
@@ -1,4 +1,4 @@
- #!/usr/bin/python
+#!/usr/bin/python
 ###########################################################################
 #          (C) Vrije Universiteit, Amsterdam (the Netherlands)            #
 #                                                                         #
